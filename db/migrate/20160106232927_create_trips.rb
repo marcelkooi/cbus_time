dbec752927ebf371c679a0308910e9dba999ab15
Marcel <1marcelkooi@gmail.com>
2016-01-08 21:05:13 -0500

    first commit

---
+++ db/migrate/20160106232927_create_trips.rb
@@ -0,0 +1,14 @@
+class CreateTrips < ActiveRecord::Migration
+  def change
+    create_table :trips do |t|
+    	t.string :type
+    	t.datetime :start_time
+    	t.string :start_location
+    	t.datetime :end_time
+    	t.string :end_location
+    	t.text :notes
+
+      t.timestamps null: false
+    end
+  end
+end
