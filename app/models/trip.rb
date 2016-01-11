5f40d821871ec744932aaf722d7ec4fb2279c3a7
marcel <mkooi@covermymeds.com>
2016-01-10 19:08:17 -0500

    created method to calculate average trip times by type

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,27 @@
 class Trip < ActiveRecord::Base
+
+validates :commute, presence: true
+
+
+
+	def self.average_time(commute_type)
+		if commute_type == "To Work"
+			@trips = Trip.where("commute = ?", "To Work")
+		elsif commute_type == "To Home"
+			@trips = Trip.where("commute = ?", "To Home")
+		else
+			@trips = Trip.all
+		end
+
+		@averages = 0
+
+		@trips.each do |trip|
+			@averages += trip.end_time - trip.start_time
+		end
+
+		@trip_average = @averages / @trips.count
+
+	end
+
+
 end
