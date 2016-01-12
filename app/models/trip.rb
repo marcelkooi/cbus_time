d3ee46576ff3c3ac43456d5ba3f164929cd7ce36
marcel <mkooi@covermymeds.com>
2016-01-11 21:49:38 -0500

    added model methods

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,7 +2,27 @@ class Trip < ActiveRecord::Base
 
 validates :commute, presence: true
 
+	# move to separate module
+	# def sec_to_min(seconds) 
+	# 	mm, ss = seconds.divmod(60)
+	# 	"%d:%d" %[mm, ss]
+	# end
 
+	def completed?
+		true if start_time && end_time
+	end
+
+	def duration
+		end_time - start_time if completed?
+	end
+
+	def clock_time(input)
+		if input == "start" && start_time
+			start_time.strftime("%H:%M:%S")
+		elsif input == "end" && end_time
+			end_time.strftime("%H:%M:%S")
+		end
+	end
 
 	def self.average_time(commute_type)
 		if commute_type == "To Work"
@@ -13,18 +33,19 @@ validates :commute, presence: true
 			@trips = Trip.all
 		end
 
-		@averages = 0
-		@trip_count = 0
+		if @trips.present?
+			@averages = 0
+			@trip_count = 0
 
-		@trips.each do |trip|
-			if trip.end_time.present?
-				@averages += trip.end_time - trip.start_time
-				@trip_count += 1
+			@trips.each do |trip|
+				if trip.completed?
+					@averages += trip.duration
+					@trip_count += 1
+				end
 			end
+
+			@trip_average = @averages / @trip_count
 		end
-
-		@trip_average = @averages / @trip_count if @trips.present?
-
 	end
 
 
